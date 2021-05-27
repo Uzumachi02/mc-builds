016fd877469d983936ff2f2ca5f3a8b2ebddaf3e
Uzumachi <uzumachi02@gmail.com>
2021-05-27 13:15:59 +0300

    create migration script

---
+++ Uzumachi.McBuilds.Migrations/Scripts/createMigrate.fsx
@@ -0,0 +1,8 @@
+open System
+open System.IO
+
+let args = Environment.GetCommandLineArgs()
+let now = DateTime.Now
+let filename = now.ToString("yyyyMMddHHmmss")
+
+File.Create($"{filename}_{args.[2]}.sql")
