70cf2c800afb3cbd326d31a22f5bde22ac9b8776
Uzumachi <uzumachi02@gmail.com>
2021-06-11 19:00:51 +0300

    add post attachments db table

---
+++ Uzumachi.McBuilds.Migrations/Scripts/20210611183142_table_post_attachments_create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "public"."post_attachments" (
+  "id" serial4,
+  "attachment_type_id" int4 NOT NULL,
+  "user_id" int4 NOT NULL,
+  "post_id" int4 NOT NULL,
+  "value" varchar NOT NULL,
+  "params" json,
+  "is_deleted" bool DEFAULT false,
+  "create_date" timestamp DEFAULT now(),
+  "delete_date" timestamp,
+  PRIMARY KEY ("id")
+)
+;
